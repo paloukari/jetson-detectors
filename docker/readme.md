23c9456f8f939f8573bfe9a686301b444fd05db1
paloukari <paloukari@hotmail.com>
2019-10-02 00:04:49 +0000

    tensorflow container

--- docker/readme.md
+++ docker/readme.md
@@ -6,11 +6,11 @@ sudo docker build . -f ./docker/Dockerfile.opencv-detector -t opencv-detector
 sudo docker run --rm --runtime nvidia --privileged -ti -e DISPLAY=$DISPLAY -v "$PWD":/src -p 32001:22 opencv-detector
 
 # To build and run the tensorflow container
-sudo docker build . -f ./docker/Dockerfile.tensorflow-detector-t tensorflow-detector
+sudo docker build . -f ./docker/Dockerfile.tensorflow-detector -t tensorflow-detector
 sudo docker run --rm --runtime nvidia --privileged -ti -e DISPLAY=$DISPLAY -v "$PWD":/src -p 32001:22 tensorflow-detector
 
 # To build and run the tensorrt detector
-sudo docker build . -f ./docker/Dockerfile.tensorrt-detector-t tensorrt-detector
+sudo docker build . -f ./docker/Dockerfile.tensorrt-detector -t tensorrt-detector
 sudo docker run --rm --runtime nvidia --privileged -ti -e DISPLAY=$DISPLAY -v "$PWD":/src -p 32001:22 tensorrt-detector
 
 > Run from the root folder of the repo
--- src/tensorflow-detector/detector.py
+++ src/tensorflow-detector/detector.py
@@ -1,34 +1,100 @@
-import cv2 as cv
+import numpy as np
 import os
+import time
+import uuid
+import cv2 as cv
+import tensorflow as tf
+import tensorflow.contrib.tensorrt as trt
+import threading
+
+from videocaptureasync import VideoCaptureAsync
+from objectdetectasync import ObjectDetectionAsync
 
 if 'CAMERA_ID' in os.environ:
     camera_id = int(os.environ['CAMERA_ID'])
 else:
     camera_id = 1
 
-classifier_xml = './src/model/haarcascade_frontalface_default.xml'
-classifier = cv.CascadeClassifier()
+if 'DETECTOR_MODEL' in os.environ:
+    detector_model = int(os.environ['DETECTOR_MODEL'])
+else:
+    detector_model = './model/ssd_inception_v2_coco_trt.pb'
 
-if not classifier.load(classifier_xml):
-    print(f'Could not load {classifier_xml}')
+if not os.path.exists(detector_model):
+    raise ValueError(f'Could not load {detector_model}')
 
-video_capture = cv.VideoCapture(camera_id)
+#video_capture = cv.VideoCapture(camera_id)
+video_capture = VideoCaptureAsync(camera_id)
+video_capture.start()
+video_capture_result, frame = video_capture.test()
+
+if video_capture_result == False:
+    raise ValueError(f'Error reading the frame from camera {camera_id}')
+
+trt_graph = tf.GraphDef()
+
+print(f'Loading model {detector_model}... (this might take some minutes)')
 
 
-while(True):
+with tf.gfile.GFile(detector_model, 'rb') as f:
+    trt_graph.ParseFromString(f.read())
+    print(f'{detector_model} loaded.')
+
+tf_config = tf.ConfigProto()
+tf_config.gpu_options.allow_growth = True
+
+tf_sess = tf.Session(config=tf_config)
+
+print(f'Importing Graph..')
+tf.import_graph_def(trt_graph, name='')
+
+tf_input = tf_sess.graph.get_tensor_by_name('image_tensor:0')
+tf_scores = tf_sess.graph.get_tensor_by_name('detection_scores:0')
+tf_boxes = tf_sess.graph.get_tensor_by_name('detection_boxes:0')
+tf_classes = tf_sess.graph.get_tensor_by_name('detection_classes:0')
+tf_num_detections = tf_sess.graph.get_tensor_by_name('num_detections:0')
+
+camera_height, camera_width, channels = frame.shape
+read_lock = threading.Lock()
+
+detector_frame = None
+
+
+def frame_callback():
+    with read_lock:
+        if detector_frame is not None:
+            return detector_frame.copy()
+        return None
+
+
+object_detection = ObjectDetectionAsync(
+    tf_sess, tf_input, tf_scores, tf_boxes, tf_classes, tf_num_detections, frame_callback)
+object_detection.start()
+
+while(video_capture_result):
     # Capture frame-by-frame
-    video_capture_result, frame = video_capture.read()
+    video_capture_result, frame, frame_resized = video_capture.read()
+    with read_lock:
+        detector_frame = frame_resized
 
     if video_capture_result == False:
         print(f'Error reading the frame from camera {camera_id}')
 
-    # face detection and other logic goes here
-    faces = classifier.detectMultiScale(frame, 1.3, 5)
+    scores, boxes, classes, num_detections = object_detection.read()
+    for i in range(int(num_detections)):
+        box = boxes[i] * np.array([camera_height,
+                                   camera_width, camera_height, camera_width])
+        box = box.astype(int)
 
-    for (x, y, w, h) in faces:
-        # send each face in mqtt topic
-        cv.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 0), thickness=2)
+        cv.rectangle(frame, (box[1], box[0]), (box[3],
+                                               box[2]), color=(0, 255, 0), thickness=1)
+        text = "{0:.0f}% | {1}".format(scores[i]*100, str(int(classes[i])))
+        cv.putText(frame,text,(box[3]+10, box[2]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
     cv.imshow('Input', frame)
     if cv.waitKey(1) == 27:
         break
+
+cv.destroyAllWindows()
+video_capture.stop()
+object_detection.stop()
